30dda6a2e4421d50db3bc1c993b3652368a602b7
Abdulahi Mohamud <abdulahimohamud22@gmail.com>
2023-12-03 12:40:29 -0800

    doing the main layout for the webpage code is functionality is still broken further testing needed

---
+++ app/clientComponents/getStartedButton.tsx
@@ -0,0 +1,15 @@
+'use client';
+import React from 'react';
+
+const GetStartedButton = () => {
+    return (
+        <>
+            {/* Adding some space below the button using margin */}
+            <div style={{ marginTop: '30px' }}>
+                <button className="btn btn-primary">Get Started</button>
+            </div>
+        </>
+    );
+};
+
+export default GetStartedButton;
---
+++ app/login/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const LoginForm = () => {
+    return(
+        <div className="text-size-container text-center text">
+            <h1 className="text-size-lg">
+                this page is going to be the login form
+            </h1>
+        </div>
+    )
+
+}
+
+export default LoginForm;
